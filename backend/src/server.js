c145d4777ae3c893fc47eefc014f2a1e14d6708d
Luis Carlos Ortellado Cabral <luiscarlosortecabral@gmail.com>
2019-11-14 23:24:31 -0300

    rutas frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+//onst socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
+//io.on('connection', socket => {
+    //console.log('Usuario connectado', socktet.id);
+//});
+
 const app = express();
+
 
 mongoose.connect('mongodb://carlos:19911991carlos@lcstore-shard-00-00-3iayj.mongodb.net:27017,lcstore-shard-00-01-3iayj.mongodb.net:27017,lcstore-shard-00-02-3iayj.mongodb.net:27017/proyecto1?ssl=true&replicaSet=lcstore-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,7 +27,8 @@ mongoose.connect('mongodb://carlos:19911991carlos@lcstore-shard-00-00-3iayj.mong
 // req.params = Acessar route params (para edit, delete)
 // req.body = Acessar corpo da requisicion (para crear, edit)
 
+app.use(cors());
 app.use(express.json());
+//app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-
 app.listen(3333);
