ea2e5fc95bb94fdb7b1f09c607cf2ce8dec14939
Luis Carlos Ortellado Cabral <luiscarlosortecabral@gmail.com>
2019-12-30 12:59:24 -0300

    frontend vista spots

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
-//onst socketio = require('socket.io');
+const socketio = require('socket.io');
+const http = require('http');
 
 
 const routes = require('./routes');
@@ -12,7 +13,14 @@ const routes = require('./routes');
     //console.log('Usuario connectado', socktet.id);
 //});
 
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log('Usuario Conectado', socket.id);
+});
 
 
 mongoose.connect('mongodb://carlos:19911991carlos@lcstore-shard-00-00-3iayj.mongodb.net:27017,lcstore-shard-00-01-3iayj.mongodb.net:27017,lcstore-shard-00-02-3iayj.mongodb.net:27017/proyecto1?ssl=true&replicaSet=lcstore-shard-0&authSource=admin&retryWrites=true&w=majority', {
@@ -31,4 +39,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { socketio } from 'socket.io-client';
 import api from '../../services/api';
 
+import './styles.css';
 
 export default function Dashboard() {
-    const [spots, setSpots]= useState([]);
-    
+    const [spots, setSpots]  = useState([]);
 
+    useEffect(() => {
+        const socket = socketio('http://localhost:3333');
+
+        socket.on('Hello', data => {
+            console.log(data);
+        })
+    }, []);
+   
     useEffect(() => {
        async function loadSpots() {
            const user_id = localStorage.getItem('user');
@@ -22,17 +32,20 @@ export default function Dashboard() {
     }, []);
 
     return (
-
         <>
-            <ul className="spot-list">
-              {spots.map(spot => (
-                  <li key={spot._id}>
-                      <header style={{ backgrounImage: `url(${spot.thumbnail_url})`}} />
-                      <strong>{spot.company}</strong>
-                      <span>{spot.price}</span>
-                  </li>
-              ))}
-            </ul>
+        <ul className="spot-list">
+            {spots.map(spot => (
+                <li key={spot._id}>
+                    <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}}/>
+                    <strong>{spot.company}</strong>
+                    <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+                   
+                </li>
+            ))}
+        </ul>
+        <Link to="/new">
+           <button className="btn"> Agregar Nuevo Spot</button>
+        </Link>
         </>
     )
 
